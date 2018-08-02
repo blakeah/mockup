0cf132abde79ca047fdef33464604d1debdc626e
blakeah <blakeah@hawaii.edu>
2018-08-02 13:37:55 -1000

    Update Alpha 0.7

--- src/main/resources/static/javascript/actors.controller.js
+++ src/main/resources/static/javascript/actors.controller.js
@@ -1,43 +1,43 @@
 // Element locators
-var faveApp = angular.module('faveApp', ['ui.bootstrap', 'limitTo']);
+var actorApp = angular.module('actorApp', ['ui.bootstrap', 'limitTo']);
 
 function actorsController($scope, $http, $uibModal) {
 
     // Getting Local JSON Data
     $http.get("http://localhost:8080/dev/api/actors").
-	    then(function(response) {
-	        $scope.actors = response.data;
-            // $scope.traits = response.data;
-	});
+    then(function(response) {
+        $scope.actors = response.data;
+        // $scope.traits = response.data;
+    });
 
-	// Getting External JSON Data
+    // Getting External JSON Data
 
-	// $http.get("https://raw.githubusercontent.com/blakeah/mockup/master/src/main/resources/data.json").
-	// then(function(response) {
-	// 	$scope.actors = response.data;
-	// 	$scope.traits = response.data;
-	// });
+    // $http.get("https://raw.githubusercontent.com/blakeah/mockup/master/src/main/resources/data.json").
+    // then(function(response) {
+    // 	$scope.actors = response.data;
+    // 	$scope.traits = response.data;
+    // });
 
-	// Order actors function
+    // Order actors function
 
-	$scope.orderByMe = function(m) {
-		$scope.myOrderBy = m;
-	};
+    $scope.orderByMe = function(m) {
+        $scope.myOrderBy = m;
+    };
 
-	// Target Individual Actor to Display in Modal
+    // Target Individual Actor to Display in Modal
 
-	$scope.view = function(a) {
-		$scope.a = a;
-		var modalInstance = $uibModal.open({
-			animation: true,
-			templateUrl: 'actorView',
-			scope: $scope,
-			size: 'lg',
-		});
-		$scope.close = function() {
-			modalInstance.close();
-		};
+    $scope.view = function(a) {
+        $scope.a = a;
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'actorView',
+            scope: $scope,
+            size: 'lg',
+        });
+        $scope.close = function() {
+            modalInstance.close();
+        };
 
-	};
+    };
 }
-faveApp.controller("actorsController", actorsController);
+actorApp.controller("actorsController", actorsController);
